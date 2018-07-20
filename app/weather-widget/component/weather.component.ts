0dd47d8d54910c7eec0a8f6fa9a949a4cc68c207
Dustin Freeman <dustin.rdf@gmail.com>
2018-07-19 22:20:51 -0500

    Implemented gnOnInit(), and interpolated data to the html

--- app/weather-widget/component/weather.component.ts
+++ app/weather-widget/component/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { Weather } from "../model/weather";
 import { WeatherService } from "../service/weather.service";
@@ -11,18 +11,36 @@ import { WeatherService } from "../service/weather.service";
   templateUrl: "weather.component.html",
 } )
 
-export class WeatherComponent {
-  private pos: Position;
+export class WeatherComponent implements OnInit {
+  public pos: Position;
   private weatherData = new Weather( null, null, null, null, null );
 
-  constructor( private service: WeatherService ) {
+  constructor( private service: WeatherService ) {  }
+
+  public ngOnInit() {
+    this.getCurrentLocation();
+  }
+
+  public getCurrentLocation() {
     this.service.getCurrentLocation()
       .subscribe( ( position ) => {
         this.pos = position;
-        this.service.getCurrentWeather(this.pos.coords.latitude, this.pos.coords.longitude)
-          .subscribe( ( weather ) => console.log( weather ),
-            ( err ) => console.error( err ) );
-    },
-    ( err ) => console.error( err ) );
+        this.getCurrentWeather();
+      },
+      ( err ) => console.error( err ) );
   }
+
+  public getCurrentWeather() {
+    this.service.getCurrentWeather( this.pos.coords.latitude, this.pos.coords.longitude )
+      .subscribe( ( weather ) => {
+        this.weatherData.temp = weather["currently"]["temperature"],
+        this.weatherData.wind = weather["currently"]["windSpeed"],
+        this.weatherData.humidity = weather["currently"]["humidity"],
+        this.weatherData.icon = weather["currently"]["icon"],
+        this.weatherData.summary = weather["currently"]["summary"],
+        console.log( "Weather: ", this.weatherData ); //TODO Remove
+      },
+      ( err ) => console.error( err ) );
+  }
+
 }
